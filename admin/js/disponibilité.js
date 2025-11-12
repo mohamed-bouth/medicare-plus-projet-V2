e7b91c9a373ed80863098853f3e659b0094a0659
Meski Abdelilah <meskiabdelilah10@gmail.com>
2025-11-12 11:00:44 +0100

    abdou

--- admin/js/disponibilité.js
+++ admin/js/disponibilité.js
@@ -1,30 +1,26 @@
-let btn = document.querySelector(".btn");
-let successMsg = document.getElementById("successMsg")
-let selectMedcin = document.getElementById("selectMedcin")
-let checkboxes = document.querySelectorAll(#jour input[type='checkbox'])
+const toggle = document.getElementById("profileToggle");
+const dropdownMenu = document.getElementById("dropdownMenu");
 
-btn.addEventListener("click", function () {     
-    let medcin = selectMedcin.value;
-    let jours = false ;
+dropdownMenu.classList.toggle('show');
 
-    if (medcin === "") {
-        successMsg.textContent = "Veuillez choisir un médecin !";
-        successMsg.classList.remove("d-none")
-        successMsg.classList.remove("bg-success-subtle")
-        successMsg.classList.add("bg-warning")
-    } else if (checkboxes.forEach(function (checkbox) {
-        if (checkboxes.checkbox) {
-            jours=true
-        }
-        successMsg.textContent = ""
-    })) {
-        
-    } else {
-        successMsg.classList.remove("d-none");
-        successMsg.classList.remove("bg-warning")
-        successMsg.classList.add("bg-success-subtle")
-    }
-    setTimeout(() => {
-        successMsg.classList.add("d-none");
-    }, 3000);
-})
+
+
+
+function toggleMenu() {
+    dropdownMenu.classList.toggle('show');
+}
+
+profileToggle.addEventListener('click', function (event) {
+    event.preventDefault();
+    toggleMenu();
+});
+
+
+let selectMedcin = getElementById("selectMedcin");
+let doctors = JSON.parse(localStorage.getItem("doctors")) || []
+
+doctors.forEach(element => {
+    const option = document.createElement("option");
+    selectMedcin.appendChild(option)
+    option.value=element.name;
+});
